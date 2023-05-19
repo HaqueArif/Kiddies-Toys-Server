669e94b6a0ff40da985fcbee082cfd74de0b302e
HaqueArif <haquearif143@gmail.com>
2023-05-19 14:32:16 +0600

    created all toys api

--- index.js
+++ index.js
@@ -28,6 +28,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db('littleWonderToys').collection('toys');
+
+    app.get('/allToys', async(req, res) => {
+      const cursor = toyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
